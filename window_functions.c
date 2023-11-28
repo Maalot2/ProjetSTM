dbcbfd9aa9eb6a32756ac4c9b3e19e768167c7ca
jackb <jackbardagji@gmail.com>
2023-11-28 22:07:10 +0100

    creating cosine_window function

--- window_functions.c
+++ window_functions.c
@@ -2,4 +2,35 @@
 // Created by jackb on 28/11/2023.
 */
 
+
 #include "window_functions.h"
+#include <complex.h>
+#include <math.h>
+
+/*!
+ * Defining cosinus window function useful for many other windows.
+ * \param[in] w Pointer to the array where the cosine window will be stored
+ * \param[in] n Length of the window
+ * \param[in] coeff Pointer to the array of coefficients
+ * \param[in] ncoeff Number of coefficients in the coeff array
+ * \param[in] sflag Boolean flag; if true, the length of the window is n - 1, otherwise, it is n
+ *
+ * \return nothing
+ */
+void cosine_window(double * w, unsigned n, const double * coeff, unsigned ncoeff, bool sflag){
+    if (n == 1){
+        // Special case for n == 1.
+        w[0] = 1.0;
+    }
+    else{
+        const unsigned wlength = sflag ? (n - 1) : n; //If sflag is true, then the value of wlength is set to (n - 1).If sflag is false, then the value of wlength is set to n
+
+        for (unsigned i = 0; i < n; ++i){
+            double wi = 0.0;
+            for (unsigned j = 0; j < ncoeff; ++j){
+                wi += coeff[j] * cos(i * j * 2.0 * M_PI / wlength);
+            }
+            w[i] = wi;
+        }
+    }
+}
--- window_functions.h
+++ window_functions.h
@@ -5,4 +5,18 @@
 #ifndef PROJETSTM_WINDOW_FUNCTIONS_H
 #define PROJETSTM_WINDOW_FUNCTIONS_H
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846264338327
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+
+void cosine_window(double * w, unsigned n, const double * coeff, unsigned ncoeff, bool sflag);
+
+}
+
 #endif //PROJETSTM_WINDOW_FUNCTIONS_H*/
