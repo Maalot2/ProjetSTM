be4115a524cebfd48c6449b9a7aef833943715a4
alexandreguignard <alexandre.guignard@insa-rennes.fr>
2023-12-05 14:41:35 +0100

    Findpeaks threshold dynamic
    Ajout du threshold dynamic. Il faudrait aussi ajouter un MinPeak dynamic car les amplitudes, en fonction du signal, peuvent varier énormément.

--- findpeaks.h
+++ findpeaks.h
@@ -5,14 +5,14 @@
 #include <stdlib.h>
 
 #define LENGTHYIN 1024
-//#define ALPHA 0.0001  // Constante d'ajustement pour la moyenne mobile exponentielle
+#define ALPHA 0.0001  // Constante d'ajustement pour la moyenne mobile exponentielle
 
 void findpeaks(float *Yin, int *iPk);
 void getAllPeaks(float *Yin, int *iPk);
 void removePeaksBelowMinPeakHeight(float *Yin, int *iPk, float MinPeakHeight);
-void removePeaksBelowThreshold(float *Yin, int *iPk, float Threshold);
+void removePeaksBelowThreshold(float *Yin, int *iPk);
 void findLocalMaxima(float *Yin, int *iPk);
-//float calculateInitialThreshold(float *Yin);
-//float updateExpMovingAverage(float newValue, float dynamicThreshold);
+float calculateInitialThreshold(float *Yin);
+float updateExpMovingAverage(float newValue, float dynamicThreshold);
 
 #endif // FINDPEAKS_H
